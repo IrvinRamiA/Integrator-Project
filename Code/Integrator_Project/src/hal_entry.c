ec213bfdcbf4a04ccadcd718be2c8349b2fff1a8
JuanLNH <52579272+JuanLNH@users.noreply.github.com>
2019-09-13 17:17:48 -0500

    Nits fixed

--- Code/Integrator_Project/src/hal_entry.c
+++ Code/Integrator_Project/src/hal_entry.c
@@ -11,8 +11,8 @@
 int adcData = 0;
 int percentage = 0;
 
-void hal_entry(void) {
-
+void hal_entry(void)
+{
     g_adc0.p_api->open(g_adc0.p_ctrl,  g_adc0.p_cfg);
     g_adc0.p_api->scanCfg(g_adc0.p_ctrl, g_adc0.p_channel_cfg);
     g_adc0.p_api->scanStart(g_adc0.p_ctrl);
@@ -26,49 +26,3 @@ void hal_entry(void) {
         percentage = (adcData * 100) / 255;
     }
 }
-
-/*
-time = 0
-samplingTime = 0
-
-time = getTimeInMs();
-samplingTime = time - previousTime;
-
-if(samplingTime >= 100)
-{
-    measuredValue = getRPM();
-    error = setPoint - measuredValue;
-    derivativeError = (error - previousError) / 10;
-    integrativeError += error * 10;
-
-    P = 0.5 * error;
-    I = 0.5 * integrativeError;
-    D = 0.5 * derivativeError;
-
-    U = P + I + D;
-
-    if(U < 0)
-    {
-        U = 0;
-    }
-    else if (U > 255)
-    {
-        U = 255;
-    }
-
-    if(setPoint < 0)
-    {
-       setPWM(Pin A, U);
-    }
-    else if(setPoint >= 0)
-    {
-       setPWM(Pin B, U);
-    }
-
-    // Display the values
-
-    previousError = error;
-    previousTime = time;
-    measuredValue = 0;
-}
-*/
